be44946a067957bbf61b0d38d99e933078101ca7
Andrey Oleynik <aoleynik@luxoft.com>
2017-08-17 12:21:41 +0300

    Removes unused parameters from .ini file and obsolete test

--- src/components/functional_module/test/src/settings_test.cc
+++ src/components/functional_module/test/src/settings_test.cc
@@ -51,16 +51,6 @@ TEST(Settings, ReadParamBoolWithSectionFail) {
   ASSERT_TRUE(launch);
 }
 
-TEST(Settings, ReadParamStringWithSectionSuccess) {
-  Settings set;
-  set.ChangeConfigFile("test.ini");
-  std::string file_path;
-  bool result = set.ReadParameter<std::string>(
-      "Remote Control", "InteriorVDCapabilitiesFile", &file_path);
-  ASSERT_TRUE(result);
-  ASSERT_EQ("./plugins/InteriorVehicleDataCapabilities.json", file_path);
-}
-
 TEST(Settings, ReadParamStringWithSectionFail) {
   Settings set;
   set.ChangeConfigFile("test.ini");
